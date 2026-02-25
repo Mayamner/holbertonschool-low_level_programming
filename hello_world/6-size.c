e69fe78cae98c311e092bd77d4b55e127595af69
Mayamner <mem.kpope@gmail.com>
2026-02-25 21:20:29 +0000

    K

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: Prints a string using puts.
+ * Description: Prints the size of various types on the computer.
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -11,8 +11,9 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
+	printf("Size of a long long int: %lu byte(s)\n",
+	       (unsigned long)sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 
 	return (0);
-}
+}i
