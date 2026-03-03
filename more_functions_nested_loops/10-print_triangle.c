25746565c43c0a41367fbf3b380d31521688fe25
Mayamner <mem.kpope@gmail.com>
2026-03-03 19:51:55 +0000

    final task

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+	int row, column, spaces;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (spaces = size - row; spaces > 0; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (column = 1; column <= row; column++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
