4f5e9e2fc50160b09de828362130c343167b880e
Mayamner <mem.kpope@gmail.com>
2026-03-04 18:04:38 +0000

    D:

--- pointers_arrays_strings/6-main.c
+++ pointers_arrays_strings/6-main.c
@@ -1,10 +1,13 @@
 #include "main.h"
+#include <stdio.h>
 
 int main(void)
 {
-	char *str;
+	char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0 123456hello world\thello world.hello world\n";
+	char *ptr;
 
-	str = "0123456789";
-	puts2(str);
+	ptr = cap_string(str);
+	printf("%s", ptr);
+	printf("%s", str);
 	return (0);
 }
