f6f60eda1a3e62f2538d89661f2ba0d6e598d112
Mayamner <mem.kpope@gmail.com>
2026-03-04 10:44:18 +0000

    final

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: pointer to the string to convert
+ *
+ * Return: the integer value of the string
+ */
+int _atoi(char *s)
+{
+	int sign;
+	int result;
+
+	sign = 1;
+	result = 0;
+	while (*s != '\0')
+	{
+		if (*s == '-')
+			sign = sign * -1;
+		if (*s >= '0' && *s <= '9')
+		{
+			result = result * 10 + (*s - '0');
+		}
+		s++;
+	}
+	return (sign * result);
+}
---
+++ pointers_arrays_strings/100-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int nb;
+
+	nb = _atoi("98");
+	printf("%d\n", nb);
+	nb = _atoi("-402");
+	printf("%d\n", nb);
+	nb = _atoi("  -98");
+	printf("%d\n", nb);
+	nb = _atoi("214748364");
+	printf("%d\n", nb);
+	nb = _atoi("0");
+	printf("%d\n", nb);
+	nb = _atoi("Suite 402");
+	printf("%d\n", nb);
+	return (0);
+}
