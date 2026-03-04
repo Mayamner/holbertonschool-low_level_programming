67e1685ab58e2eabc5526355b1d80d44c0dd8358
Mayamner <mem.kpope@gmail.com>
2026-03-04 10:32:33 +0000

    fixed

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,21 +1,27 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string
+ * puts_half - prints the second half of a string
  * @str: pointer to the string to print
  *
  * Return: void
  */
-void puts2(char *str)
+void puts_half(char *str)
 {
-	int i;
+	int len;
+	int start;
 
-	i = 0;
-	while (str[i] != '\0')
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+	if (len % 2 == 0)
+		start = len / 2;
+	else
+		start = (len + 1) / 2;
+	while (str[start] != '\0')
 	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
-		i++;
+		_putchar(str[start]);
+		start++;
 	}
 	_putchar('\n');
 }
