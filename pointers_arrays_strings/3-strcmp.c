04b90080d7fe9a0426e9a3a39fc898bf504e994c
Mayamner <mem.kpope@gmail.com>
2026-03-04 17:49:04 +0000

    f

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: pointer to the first string
+ * @s2: pointer to the second string
+ *
+ * Return: 0 if equal, negative if s1 < s2, positive if s1 > s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
