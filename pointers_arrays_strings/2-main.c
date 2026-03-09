84ea3b415be3eef2199c981b45a45240abc71522
Mayamner <mem.kpope@gmail.com>
2026-03-09 20:04:45 +0000

    444

--- pointers_arrays_strings/2-main.c
+++ pointers_arrays_strings/2-main.c
@@ -19,3 +19,14 @@ int main(void)
 	printf("%s", ptr);
 	return (0);
 }
+
+int main(void)
+{
+	char *s = "hello";
+	char *f;
+
+	f = _strchr(s, 'l');
+	if (f != NULL)
+		printf("%s\n", f);
+	return (0);
+}
