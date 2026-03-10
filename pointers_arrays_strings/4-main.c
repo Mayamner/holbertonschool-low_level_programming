62cf8f65695232edec35d4fb0ad983c02d6a4975
Mayamner <mem.kpope@gmail.com>
2026-03-10 16:42:39 +0000

    d

--- pointers_arrays_strings/4-main.c
+++ pointers_arrays_strings/4-main.c
@@ -37,3 +37,13 @@ int main(void)
 	print_array(a, sizeof(a) / sizeof(int));
 	return (0);
 }
+int main(void)
+{
+	char *s = "hello, world";
+	char *f = "world";
+	char *t;
+
+	t = _strpbrk(s, f);
+	printf("%s\n", t);
+	return (0);
+}
---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: pointer to the string to search
+ * @accept: pointer to the accepted bytes string
+ *
+ * Return: pointer to the byte in s that matches one of the bytes in accept,
+ * or NULL if no such byte is found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s != '\0')
+	{
+		i = 0;
+		while (accept[i] != '\0')
+		{
+			if (*s == accept[i])
+				return (s);
+			i++;
+		}
+		s++;
+	}
+	return (0);
+}
