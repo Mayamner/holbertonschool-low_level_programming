e486d6af601689d68e341f7c4f400bcf63083f1c
Mayamner <mem.kpope@gmail.com>
2026-03-10 17:26:45 +0000

    E

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -11,3 +11,14 @@ int main(void)
 	printf("%s", str);
 	return (0);
 }
+
+int main(void)
+{
+	char *s = "hello, world";
+	char *f = "world";
+	char *t;
+
+	t = _strstr(s, f);
+	printf("%s\n", t);
+	return (0);
+}
