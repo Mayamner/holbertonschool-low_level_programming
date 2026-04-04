00d730e4dc4bdd97244a8dacfe517ff8d3fded62
Mayamner <mem.kpope@gmail.com>
2026-04-04 19:45:12 +0300

    second to last

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,18 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdlib.h>
+
 /**
  * struct dog - defines a dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
