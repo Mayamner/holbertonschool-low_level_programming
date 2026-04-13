4e8495aa50d1f0c01d27e9f6c199729285d458f9
Mayamner <mem.kpope@gmail.com>
2026-04-13 19:15:12 +0300

    f

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,21 +1,17 @@
 #include "variadic_functions.h"
 #include <stdio.h>
-
 /**
 * print_char - prints a char
 * @args: va_list of arguments
 */
-
 void print_char(va_list args)
 {
 	printf("%c", va_arg(args, int));
 }
-
 /**
 * print_int - prints an int
 * @args: va_list of arguments
 */
-
 void print_int(va_list args)
 {
 	printf("%d", va_arg(args, int));
@@ -30,12 +26,10 @@ void print_float(va_list args)
 {
 	printf("%f", va_arg(args, double));
 }
-
 /**
 * print_string - prints a string
 * @args: va_list of arguments
 */
-
 void print_string(va_list args)
 {
 	char *str;
@@ -43,26 +37,22 @@ void print_string(va_list args)
 	str = va_arg(args, char *);
 	printf("%s", str ? str : "(nil)");
 }
-
 /**
 * print_all - prints anything
 * @format: list of types of arguments
 * Return: Nothing.
 */
-
 void print_all(const char * const format, ...)
 {
 	va_list args;
 	unsigned int i;
 	int sep;
 	int j;
-
-	/**
+/**
 * struct fmt - struct for format types
 * @type: the format character
 * @f: the function associated
 */
-
 	struct fmt
 	{
 		char type;
