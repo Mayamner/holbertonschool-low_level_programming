e00347c3a4289467d748931bbe3b781e8db8c381
Mayamner <mem.kpope@gmail.com>
2026-04-13 17:50:56 +0300

    new task

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,6 +1,5 @@
 #include "variadic_functions.h"
 #include <stdio.h>
-#include <string.h>
 
 /**
  * print_all - prints anything
@@ -13,32 +12,24 @@ void print_all(const char * const format, ...)
 	va_list args;
 	unsigned int i;
 	char *str;
-	char *types = "cifs";
 	int sep;
+	char fmt;
 
 	va_start(args, format);
 	i = 0;
 	sep = 0;
 	while (format && format[i])
 	{
-		str = strchr(types, format[i]);
-		if (str)
-		{
-			if (sep)
-				printf(", ");
-			if (format[i] == 'c')
-				printf("%c", va_arg(args, int));
-			if (format[i] == 'i')
-				printf("%d", va_arg(args, int));
-			if (format[i] == 'f')
-				printf("%f", va_arg(args, double));
-			if (format[i] == 's')
-			{
-				str = va_arg(args, char *);
-				printf("%s", str ? str : "(nil)");
-			}
-			sep = 1;
-		}
+		fmt = format[i];
+		str = (fmt == 's') ? va_arg(args, char *) : NULL;
+		if (fmt == 'c')
+			printf("%s%c", sep++ ? ", " : "", va_arg(args, int));
+		if (fmt == 'i')
+			printf("%s%d", sep++ ? ", " : "", va_arg(args, int));
+		if (fmt == 'f')
+			printf("%s%f", sep++ ? ", " : "", va_arg(args, double));
+		if (fmt == 's')
+			printf("%s%s", sep++ ? ", " : "", str ? str : "(nil)");
 		i++;
 	}
 	va_end(args);
