e9ca8db564e8dd92247a9aaf9824a013c9300321
Mayamner <mem.kpope@gmail.com>
2026-04-13 18:10:46 +0300

    new task

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -51,6 +51,11 @@ void print_all(const char * const format, ...)
 	unsigned int i;
 	int sep;
 	int j;
+    /**
+	 * struct fmt - struct for format types
+	 * @type: the format character
+	 * @f: the function associated
+	 */
 	struct fmt
 	{
 		char type;
