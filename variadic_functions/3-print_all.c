a03e05f03fed8c4b83a9b07714cbaebac04b0d6c
Mayamner <mem.kpope@gmail.com>
2026-04-13 19:21:31 +0300

    f

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -6,7 +6,7 @@
 */
 void print_char(va_list args)
 {
-	printf("%c", va_arg(args, int));
+printf("%c", va_arg(args, int));
 }
 /**
 * print_int - prints an int
@@ -14,7 +14,7 @@ void print_char(va_list args)
 */
 void print_int(va_list args)
 {
-	printf("%d", va_arg(args, int));
+printf("%d", va_arg(args, int));
 }
 
 /**
@@ -24,7 +24,7 @@ void print_int(va_list args)
 
 void print_float(va_list args)
 {
-	printf("%f", va_arg(args, double));
+printf("%f", va_arg(args, double));
 }
 /**
 * print_string - prints a string
@@ -32,10 +32,9 @@ void print_float(va_list args)
 */
 void print_string(va_list args)
 {
-	char *str;
-
-	str = va_arg(args, char *);
-	printf("%s", str ? str : "(nil)");
+char *str;
+str = va_arg(args, char *);
+rintf("%s", str ? str : "(nil)");
 }
 /**
 * print_all - prints anything
@@ -44,46 +43,45 @@ void print_string(va_list args)
 */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	unsigned int i;
-	int sep;
-	int j;
+va_list args;
+unsigned int i;
+int sep;
+int j;
 /**
 * struct fmt - struct for format types
 * @type: the format character
 * @f: the function associated
 */
-	struct fmt
-	{
-		char type;
-		void (*f)(va_list);
-	} types[] = {
-		{'c', print_char},
-		{'i', print_int},
-		{'f', print_float},
-		{'s', print_string},
-		{0, NULL}
-	};
-
-	va_start(args, format);
-	i = 0;
-	sep = 0;
-	while (format && format[i])
-	{
-		j = 0;
-		while (types[j].f)
-		{
-			if (format[i] == types[j].type)
-			{
-				if (sep)
-					printf(", ");
-				types[j].f(args);
-				sep = 1;
-			}
-			j++;
-		}
-		i++;
-	}
-	va_end(args);
-	printf("\n");
+struct fmt
+{
+char type;
+void (*f)(va_list);
+} types[] = {
+{'c', print_char},
+{'i', print_int},
+{'f', print_float},
+{'s', print_string},
+{0, NULL}
+};
+va_start(args, format);
+i = 0;
+sep = 0;
+while (format && format[i])
+{
+j = 0;
+while (types[j].f)
+{
+if (format[i] == types[j].type)
+{
+if (sep)
+printf(", ");
+types[j].f(args);
+sep = 1;
+}
+j++;
+}
+i++;
+}
+va_end(args);
+printf("\n");
 }
