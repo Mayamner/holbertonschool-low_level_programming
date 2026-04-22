90abd9bc1d7b017867ce2a0820214881b7299d8d
Mayamner <mem.kpope@gmail.com>
2026-04-22 17:33:38 +0300

    d

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -9,6 +9,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	unsigned int i;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
@@ -21,7 +22,10 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new->len = strlen(str);
+	for (i = 0; str[i]; i++)
+		;
+
+	new->len = i;
 	new->next = *head;
 	*head = new;
 
