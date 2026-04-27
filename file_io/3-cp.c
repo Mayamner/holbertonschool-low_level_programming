0d2c9072473a94d8aba1c9503ab6e61a308dcd55
Mayamner <mem.kpope@gmail.com>
2026-04-27 11:59:25 +0300

    next task

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -30,7 +30,7 @@ int main(int ac, char **av)
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(2, "Error: Can't write to file %s\n", av[2]);
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
 		close(fd_from);
 		exit(99);
 	}
@@ -40,7 +40,7 @@ int main(int ac, char **av)
 		nwrite = write(fd_to, buf, nread);
 		if (nwrite == -1 || nwrite != nread)
 		{
-			dprintf(2, "Error: Can't write to file %s\n", av[2]);
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
 			close(fd_from);
 			close(fd_to);
 			exit(99);
